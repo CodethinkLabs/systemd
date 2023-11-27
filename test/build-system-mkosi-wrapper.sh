ab7595041ca3642fe29b0cb05a377d90ebe21453
Richard Maw <richard.maw@codethink.co.uk>
2023-11-27 17:52:29 +0000

    WIP: test: Run integration tests with mkosi-built image
    TODO:
    1. Get all the tests running
       1. Support customising the image by activating overlays
       2. Support passing extra options to vmspawn and qemu and kernel
    2. Support running a prebuilt image instead of building with mkosi
    
    Run tests with:
    
    sudo meson test -C build/ --suite integration-tests --num-processes "$(($(getconf _NPROCESSORS_ONLN) / 2))"

---
+++ test/build-system-mkosi-wrapper.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: LGPL-2.1-or-later
+set -eux
+image="$1"
+output="$2"
+mkdir -p "$output"
+mkosi -C .. --image="$image" --format=disk --output-dir="$(readlink -f "$output")" --without-tests -fi build
